b570ae4cd2755970ba0ec41c6a1550a3d3ccbcb7
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-02 19:04:50 +0400

    Initial Commit

---
+++ I_am_broke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_broke
+//
+//  Created by ლუკა ზუხბაია on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
